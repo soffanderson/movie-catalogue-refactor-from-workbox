ad7806b56f9d9851cd6c1fdb885ba0b594214d78
soffanderson <s.btudz@gmail.com>
2023-06-05 21:22:04 +0800

    Membuat repo baru - Latihan refactor

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: 'fd8f2de6a01f4f6f3e2b5692cc6d36cc',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
+};
+
+export default CONFIG;
---
+++ src/scripts/utils/notification-helper.js
@@ -0,0 +1,43 @@
+const NotificationHelper = {
+  sendNotification({ title, options }) {
+    // TODO: check availability
+    if (!this._checkAvailability()) {
+      console.log('Notification not supported in this browser.');
+      return;
+    }
+    // TODO: check permission. if not granted, send request
+    if (!this._checkPermission()) {
+      console.log('User did not yet granted permission.');
+      this._requestPermission();
+      return;
+    }
+    // TODO: show notification
+    this._showNotification({ title, options });
+  },
+
+  _checkAvailability() {
+    return 'Notification' in window;
+  },
+
+  _checkPermission() {
+    return Notification.permission === 'granted';
+  },
+
+  async _requestPermission() {
+    const status = await Notification.requestPermission();
+
+    if (status === 'denied') {
+      console.log('Notification Denied.');
+    }
+    if (status === 'default') {
+      console.log('Notification Closed.');
+    }
+  },
+
+  async _showNotification({ title, options }) {
+    const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+    serviceWorkerRegistration.showNotification(title, options);
+  },
+};
+
+export default NotificationHelper;
